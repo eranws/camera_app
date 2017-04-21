ce05fe29ea53ce204a7315d06b5e00e7eff9e604
eranws <eranws@gmail.com>
2017-04-21 00:42:15 -0400

    camera_app skeleton + added gray and downscale to 5x5

---
+++ camera_app.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+'''
+Camera App
+
+This boilerplate code sample opens camera or video and shows a frame
+
+Usage:
+  camera_app.py [--pause] [<video source>]
+
+  --pause  -  Start with playback paused at the first video frame.
+  
+Keys:
+  Esc      - exit app
+  SPACE    - pause video
+
+ripped off from mosse.py, MOSSE tracking sample, OpenCV
+'''
+
+import numpy as np
+import cv2
+import video
+
+class App:
+    def __init__(self, video_src, paused = False):
+        self.cap = video.create_capture(video_src)
+        _, self.frame = self.cap.read()
+        cv2.imshow('frame', self.frame)
+        self.paused = paused
+
+    def run(self):
+        while True:
+            if not self.paused:
+                ret, self.frame = self.cap.read()
+                if not ret:
+                    break
+                frame_gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+
+            vis = frame_gray.copy()
+            h, w = vis.shape[:2]
+
+            A = 5
+
+            m = w/3/A
+            n = h/2/A
+
+            w1 = w/2
+            h1 = h/2
+            
+            for i in range(A):
+                for j in range(A):
+                # vis[h0:h1,w1+(i-A/2)*m:w1+(i+1-A/2)*m] = cv2.mean(vis[h0:h1,w1+(i-A/2)*m:w1+(i+1-A/2)*m])[0]
+                    vis[h1+(j-A/2)*n:h1+(j+1-A/2)*n,w1+(i-A/2)*m:w1+(i+1-A/2)*m] = cv2.mean(vis[h1+(j-A/2)*n:h1+(j+1-A/2)*n,w1+(i-A/2)*m:w1+(i+1-A/2)*m])[0]
+
+            cv2.imshow('frame', vis)
+            ch = cv2.waitKey(10)
+            if ch == 27:
+                break
+            if ch == ord(' '):
+                self.paused = not self.paused
+
+if __name__ == '__main__':
+    print __doc__
+    import sys, getopt
+    opts, args = getopt.getopt(sys.argv[1:], '', ['pause'])
+    opts = dict(opts)
+    try: video_src = args[0]
+    except: video_src = '1'
+
+    App(video_src, paused = '--pause' in opts).run()
